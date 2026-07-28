8bd3db4f561646e70fac683a9f9a513c11ebb753
Shashwat Shukla <shashwatshukklla@gmail.com>
2026-07-28 20:49:55 +0530

    Add Web Manifest and Service Worker for PWABuilder APK support

---
+++ sync-in/sw.js
@@ -0,0 +1,12 @@
+// Service worker for PWA / Android APK support
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Let the browser handle standard requests
+});
